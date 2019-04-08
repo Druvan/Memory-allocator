1681b8fe7644c5e105bdebd6c6203dcf13948e75
Druvan <edvin.lundberg@gmail.com>
2019-04-08 15:58:37 +0200

    CPU should now be working aswell (not tested)

--- makefile
+++ makefile
@@ -4,7 +4,7 @@ all: memallocator.c
 	gcc -g -Wall -o memallocator memallocator.c
 
 run: all
-	./memallocator $(tot_ram) $(mem_procent) $(time)
+	./memallocator $(tot_ram) $(mem_percent) $(time)
 	make clean
 
 clean: 
--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 test=$(cat /proc/meminfo | grep MemTotal | sed -e 's/ /\n/g' | tail -2 | head -1)
-echo $test
-make run tot_ram=$test mem_procent=50 time=10
+echo "total_ram" . $test
+cpu_percent=$1
+make run tot_ram=$test mem_percent=$2 time=$3 &
+stress-ng --cpu 1 --cpu-load $cpu_percent -t $3
