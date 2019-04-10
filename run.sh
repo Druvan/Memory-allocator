6c046e575988522fd671532f837ce5f1d3a09180
Druvan <edvin.lundberg@gmail.com>
2019-04-10 15:06:37 +0200

    Does everything in python now

--- run.sh
+++
@@ -1,4 +0,0 @@
-total_ram=$(cat /proc/meminfo | grep MemTotal | sed -e 's/ /\n/g' | tail -2 | head -1)
-cpu_percent=$1
-python3 mem-allocator.py $total_ram $2 $3 &
-stress-ng --cpu 1 --cpu-load $cpu_percent -t $3
