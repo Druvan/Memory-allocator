1a593fa5f700006f36c76913a6454faf30bfddd7
root <root@Edvin.localdomain>
2019-04-10 14:23:09 +0200

    Removed debugging

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 total_ram=$(cat /proc/meminfo | grep MemTotal | sed -e 's/ /\n/g' | tail -2 | head -1)
-echo $total_ram
 cpu_percent=$1
-python3 mem-allocator.py $total_ram $2 $3
+python3 mem-allocator.py $total_ram $2 $3 &
 stress-ng --cpu 1 --cpu-load $cpu_percent -t $3
