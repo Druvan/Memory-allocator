b2ac19a97f3faea7428121ff06c23a32d451b2ce
Druvan <edvin.lundberg@gmail.com>
2019-04-12 15:12:58 +0200

    Added missing import sys

--- memAllocatorHIO.py
+++ memAllocatorHIO.py
@@ -1,8 +1,10 @@
 from harmonicPE.daemon import listen_for_tasks
 from memAllocator import memAllocator
+import sys
 
 def process_data(message_bytes):
     ma = memAllocator(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]))
+    print()
     ma.run()
 
 
