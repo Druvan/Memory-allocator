f1768041d247cd658f0917f1c24fef999701e164
Druvan <edvin.lundberg@gmail.com>
2019-04-15 14:48:42 +0200

    testing inputs

--- memAllocatorHIO.py
+++ memAllocatorHIO.py
@@ -2,9 +2,9 @@ from harmonicPE.daemon import listen_for_tasks
 from memAllocator import memAllocator
 import sys
 
-def process_data(message_bytes):
+def process_data():
+    print(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]))
     ma = memAllocator(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]))
-    print()
     ma.run()
 
 
